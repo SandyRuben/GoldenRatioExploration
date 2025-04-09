7de26328026babe5dbb2f58629b2161126527610
SandyRuben <flier12345@gmail.com>
2025-04-08 22:33:03 -0700

    added function to clicking the mouse; now inverts the line color completed first iteration of spiral testing, created a fun semi-hypnotic sun thing. thought it was cool, but create an off branch for it.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,54 @@
+//declare global variables
+let point, lineColor, fibI;
+
+let maxFibNumber = 1000;
+let fibNumbers = [];
+
 function setup() {
-  createCanvas(400, 400);
+  //canvas created for main display
+  createCanvas(windowWidth, windowHeight);
+
+  //setting global variables
+
+  //white lines for high visability
+  lineColor = 255;
+  stroke(lineColor);
+
+  point = createVector(1,1);
+
+  //setting a black background to contrast with the white lines
+  background(0);
 }
 
 function draw() {
-  background(220);
+  //populate array 
+
+    //draw line from the center out
+    translate(width*.5, height*.5);
+    line(0,0, point.x, point.y);
+    // line(0,0, -point.x, -point.y);
+    // line(0,0, point.x/2, point.y/2);
+    // line(0,0, -point.x/2, -point.y/2);
+
+    //update point to new location
+    point.rotate(radians(137.51));
+
+    point.setMag(1000);
+}
+
+function mouseClicked() {
+  if (lineColor == 255) {
+    lineColor = 0;
+  } else {
+    lineColor = 255;
+  };
+ 
+
+  stroke(lineColor);
+}
+
+function windowResized() {
+  createCanvas(windowWidth, windowHeight);
+  translate(width*.5, height*.5);
+  background(0);
 }
